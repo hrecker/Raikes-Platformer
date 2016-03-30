bf3ee851722484b840f93fc5eacfe46b0cb56694
hrecker <henryjrecker@gmail.com>
2016-03-29 23:07:02 -0500

    created basic player and first level scene

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float jumpSpeed;
+
+    private Rigidbody2D rigidbodyObject;
+    private BoxCollider2D boxCollider;
+    private float distanceToGround;
+    private float groundMargin = 0.1f;
+    
+    void Start()
+    {
+        rigidbodyObject = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        if (boxCollider != null)
+        {
+            distanceToGround = boxCollider.bounds.extents.y;
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.Raycast(transform.position, -Vector2.up, distanceToGround + groundMargin);
+    }
+
+    public void Jump()
+    {
+        if(rigidbodyObject != null && isGrounded())
+        {
+            rigidbodyObject.velocity = new Vector2(rigidbodyObject.velocity.x, jumpSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float jumpDelay;
+
+    private Movement movement;
+    private float delayPassed;
+
+    void Start()
+    {
+        movement = GetComponent<Movement>();
+    }
+    
+    void Update()
+    {
+        delayPassed += Time.deltaTime;
+        if (Input.GetButtonDown("Fire1") && delayPassed >= jumpDelay && movement != null)
+        {
+            delayPassed = 0;
+            movement.Jump();
+        }
+    }
+}
