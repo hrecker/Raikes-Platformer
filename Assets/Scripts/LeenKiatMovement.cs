a38aea5eef1aa0e57e91677e60b42ca6ecd3686e
hrecker <henryjrecker@gmail.com>
2016-04-05 22:48:43 -0500

    added leenkiat enemy, set up its horizontal movement

---
+++ Assets/Scripts/LeenKiatMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class LeenKiatMovement : MonoBehaviour {
+
+    public Direction horizontalDirection;
+    public float normalHorizontalSpeed;
+    public float squishedHorizontalSpeed;
+    public float jumpSpeed;
+    private Rigidbody2D rigidbodyObject;
+    private float directionModifier;
+    
+    void Start ()
+    {
+        rigidbodyObject = GetComponent<Rigidbody2D>();
+        directionModifier = horizontalDirection == Direction.LEFT ? -1 : 1;
+        rigidbodyObject.velocity = new Vector2(directionModifier * normalHorizontalSpeed, rigidbodyObject.velocity.y);
+	}
+	
+	void Update ()
+    {
+        rigidbodyObject.velocity = new Vector2(directionModifier * normalHorizontalSpeed, rigidbodyObject.velocity.y);
+    }
+}
