8985ca2e64249afb40c4ba967996b6e503df4c92
Cooper Knaak <cooperknaak@gmail.com>
2016-04-09 17:39:30 -0500

    Player can now jump on MiniLeenKiatEnemy to squish it. Jumping on an enemy causes player to hop.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -36,7 +36,7 @@ public class PlayerMovement : MonoBehaviour {
 
 	public void FullHop()
 	{
-		if(rigidbodyObject != null && IsGrounded())
+		if (rigidbodyObject != null && IsGrounded())
 		{
 			rigidbodyObject.velocity = new Vector2(rigidbodyObject.velocity.x, jumpSpeed);
 		}
@@ -51,5 +51,48 @@ public class PlayerMovement : MonoBehaviour {
 
 	public void Update() {
         this.rigidbodyObject.velocity += this.acceleration * Time.deltaTime;
+	}
+
+	public void OnCollisionEnter2D(Collision2D collision) {
+		//TODO Find a way to ACTUALLY identify different types of collisions.
+		if (collision.gameObject.tag.Equals ("Untagged")) {
+			LeenKiatMovement em = collision.gameObject.GetComponent<LeenKiatMovement>();
+			if (this.landedOnObject(collision) && em != null) {
+				em.Squish ();
+				if (Input.GetKey (KeyCode.Space)) {
+					this.FullHop ();
+				} else {
+					this.ShortHop ();
+				}
+			}
+		}
+	}
+
+	public bool landedOnObject(Collision2D collision) {
+		Vector2 collisionPoint = PlayerMovement.AverageContacts (collision.contacts);
+		if (PlayerMovement.overlapIsHorizontal (collision.contacts)) {
+			float yBuffer = 1.0f;
+			float maxY = this.transform.position.y - (boxCollider.bounds.size.y / 2.0f) + yBuffer;
+			float minY = collision.transform.position.y - collision.gameObject.GetComponent<BoxCollider2D> ().bounds.size.y / 2.0f - yBuffer;
+			return minY <= collisionPoint.y && collisionPoint.y <= maxY;
+		} else {
+			return false;
+		}
+	}
+
+	public static Vector2 AverageContacts(ContactPoint2D[] contacts) {
+		Vector2 position = new Vector2();
+		foreach (var c in contacts) {
+			position += c.point;
+		}
+		return position / contacts.Length;
+	}
+
+	public static bool overlapIsHorizontal(ContactPoint2D[] contacts) {
+		if (contacts.Length <= 1) {
+			return true;
+		}
+		//Relies on there only being two contact points
+		return contacts [0].point.y == contacts [1].point.y;
 	}
 }
