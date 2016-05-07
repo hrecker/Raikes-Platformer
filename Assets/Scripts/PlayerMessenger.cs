382c96f4cd16afbdca9e979fc42c647a29439416
hrecker <henryjrecker@gmail.com>
2016-05-06 21:52:22 -0500

    Made player invincible after any hit

--- Assets/Scripts/PlayerMessenger.cs
+++ Assets/Scripts/PlayerMessenger.cs
@@ -2,6 +2,18 @@
 
 public class PlayerMessenger : MonoBehaviour, IMessenger
 {
+    public float invincibilityTime; // Time that player is invincible after getting hit
+
+    private bool invulnerable;
+    private float currentTimePassed;
+    private Hurtbox[] hurtboxes;
+
+    void Start()
+    {
+        hurtboxes = GetComponentsInChildren<Hurtbox>();
+        invulnerable = false;
+    }
+
     public void Invoke(string msg, object[] args)
     {
         switch(msg)
@@ -11,7 +23,40 @@ public class PlayerMessenger : MonoBehaviour, IMessenger
                 break;
             case "HitByOther":
                 Debug.Log("Player received hit");
+                makeInvulnerable();
                 break;
+        }
+    }
+
+    void Update()
+    {
+        if (invulnerable)
+        {
+            currentTimePassed += Time.deltaTime;
+            if(currentTimePassed >= invincibilityTime)
+            {
+                makeVulnerable();
+            }
+        }
+    }
+
+    private void makeInvulnerable()
+    {
+        currentTimePassed = 0;
+        invulnerable = true;
+        foreach(Hurtbox hurtbox in hurtboxes)
+        {
+            hurtbox.Deactivate();
+        }
+    }
+
+    private void makeVulnerable()
+    {
+        currentTimePassed = 0;
+        invulnerable = false;
+        foreach (Hurtbox hurtbox in hurtboxes)
+        {
+            hurtbox.Activate();
         }
     }
 }
