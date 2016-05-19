c2734593ab4e7d0b034f97ae267708cf72465121
hrecker <henryjrecker@gmail.com>
2016-05-18 20:16:38 -0500

    Created prefab for buzzword letter block

---
+++ Assets/Scripts/BuzzwordLetterMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuzzwordLetterMovement : MonoBehaviour {
+
+    public float speed;
+    public Direction direction;
+    private Rigidbody2D rigidbodyObject;
+
+    void Start()
+    {
+        rigidbodyObject = GetComponent<Rigidbody2D>();
+
+        rigidbodyObject.velocity = getVelocity();
+    }
+
+    void Update()
+    {
+        rigidbodyObject.velocity = getVelocity();
+    }
+
+    private Vector2 getVelocity()
+    {
+        if (direction == Direction.UP || direction == Direction.DOWN)
+        {
+            return new Vector2(0, speed * 0.5f * (float)direction);
+        }
+        else
+        {
+            return new Vector2((float)direction * speed, 0);
+        }
+    }
+}
--- Assets/Scripts/Direction.cs
+++ Assets/Scripts/Direction.cs
@@ -1,6 +1,8 @@
 ﻿public enum Direction
 {
+    DOWN = -2,
 	LEFT = -1,
     NONE = 0,
-    RIGHT = 1
+    RIGHT = 1,
+    UP = 2
 }
