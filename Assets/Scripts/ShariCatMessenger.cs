d4f213f1c4abac7df3ea85d9a1a53f3d5100a288
hrecker <henryjrecker@gmail.com>
2016-05-22 11:29:55 -0500

    Created Shari's Cat enemy

---
+++ Assets/Scripts/ShariCatMessenger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ShariCatMessenger : MonoBehaviour, IMessenger
+{
+    public void Invoke(string msg, object[] args)
+    {
+        switch (msg)
+        {
+            case "HitOther":
+                Debug.Log("Shari's cat hit another object");
+                break;
+            case "HitByOther":
+                Debug.Log("Shari's cat received hit");
+                Destroy(gameObject);
+                break;
+        }
+    }
+}
