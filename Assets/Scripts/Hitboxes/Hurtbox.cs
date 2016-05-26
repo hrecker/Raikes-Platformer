afdfe34192343d7c428cc121a16f89116825b5bd
Cooper Knaak <cooperknaak@gmail.com>
2016-05-26 18:25:46 -0500

    Removed HitboxHarmComponent and moved the functionality to Hitbox and Hurtbox classes. Updated prefabs for this change.

--- Assets/Scripts/Hitboxes/Hurtbox.cs
+++ Assets/Scripts/Hitboxes/Hurtbox.cs
@@ -6,7 +6,9 @@ public class Hurtbox : MonoBehaviour
     public ColliderBoxType boxType; // what type of collisions does this hurtbox detect
 
     private IMessenger objectMessenger;
-    private Collider2D hurtboxCollider;
+	private Collider2D hurtboxCollider;
+
+	public HitboxHarmType harmType;
 
     public Collider2D HurtboxCollider
     {
@@ -37,7 +39,7 @@ public class Hurtbox : MonoBehaviour
         Hitbox otherHitbox = other.GetComponent<Hitbox>();
         if (other.isTrigger && otherHitbox != null && objectMessenger != null &&
             (boxType == ColliderBoxType.ANY || otherHitbox.boxType == ColliderBoxType.ANY || boxType == otherHitbox.boxType) &&
-			this.canReceiveHarm(other))
+			this.canReceiveHarm(otherHitbox))
         {
             objectMessenger.Invoke("HitByOther", null);
         }
@@ -51,5 +53,10 @@ public class Hurtbox : MonoBehaviour
     public void Activate()
     {
         hurtboxCollider.enabled = true;
-    }
+	}
+
+	public bool canReceiveHarm(Hitbox hitbox) {
+		return (this.harmType & hitbox.harmType) != 0;
+	}
+
 }
