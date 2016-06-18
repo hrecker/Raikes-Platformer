3c48a85fdc36d700bd252e8991ca0c7ba8ea983f
Cooper Knaak <cooperknaak@gmail.com>
2016-06-18 18:58:05 -0500

    Fixed enemy prefabs so their hit boxes include the proper HitboxHarmType. Created PowerupBlock.

---
+++ Assets/Scripts/Messengers/PowerupBlockMessenger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class PowerupBlockMessenger: MonoBehaviour, IMessenger
+{
+	private SpriteRenderer spriteRenderer;
+
+	void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	public void Invoke(Message msg, object[] args) {
+		switch (msg) {
+		case Message.HIT_BY_OTHER:
+			Debug.Log ("Powerup spawned!");
+			break;
+		}
+	}
+}
+
--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -5,31 +5,32 @@ public class ObjectSpawner: MonoBehaviour, IDirected
 	public GameObject objectToSpawn;
 	public Vector2 spawnOffset;
 	// direction to spawn in. If NONE, then the direction of the parent object is used.
-	public HorizontalDirection spawnDirection;
+	public HorizontalDirection horizontalSpawnDirection;
+	public VerticalDirection verticalSpawnDirection;
 
     public HorizontalDirection horizontalDirection
     {
-        get { return spawnDirection; }
-        set { spawnDirection = value; }
+        get { return horizontalSpawnDirection; }
+        set { horizontalSpawnDirection = value; }
     }
 
     public VerticalDirection verticalDirection
     {
-        get { return VerticalDirection.NONE; }
-        set { }
+		get { return verticalSpawnDirection; }
+		set { verticalSpawnDirection = value; }
     }
 
     public GameObject SpawnObject(Vector2 position, Vector2 offset, HorizontalDirection spawnDirection)
     {
-        GameObject obj = Instantiate(objectToSpawn, new Vector3(position.x + offset.x * (float)spawnDirection,
-			position.y + offset.y), Quaternion.identity) as GameObject;
+        GameObject obj = Instantiate(objectToSpawn, new Vector3(position.x + offset.x * (float)horizontalSpawnDirection,
+			position.y + offset.y * (float)verticalSpawnDirection), Quaternion.identity) as GameObject;
         return obj;
 	}
 
 	public GameObject SpawnObject()
     {
 		Vector2 parentPosition = GetComponent<BoxCollider2D> ().transform.position;
-		HorizontalDirection spawnDirection = this.spawnDirection;
+		HorizontalDirection spawnDirection = horizontalSpawnDirection;
 		if (spawnDirection == HorizontalDirection.NONE)
         {
 			spawnDirection = GetComponent<IDirected> ().horizontalDirection;
