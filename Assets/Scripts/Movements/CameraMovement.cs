25129974a4e41c249b6e68fc61851d5fcd5e6e91
hrecker <henryjrecker@gmail.com>
2016-06-25 20:03:55 -0500

    Updated camera movement to not always center on the player vertically (#106)

---
+++ Assets/Scripts/Movements/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    public float baseHeight; // lowest height that this camera can go to
+    public float verticalMotionLine; // how high does the player have to be for the camera to follow it verically
+
+    void Start()
+    {
+        transform.position = new Vector3(transform.position.x, baseHeight, transform.position.z);
+    }
+
+    void Update()
+    {
+        /*if (transform.position.y < baseHeight)
+        {
+            transform.position = new Vector3(player.position.x, baseHeight, transform.position.z);
+        }*/
+        if(player.position.y >= verticalMotionLine)
+        {
+            transform.position = new Vector3(player.position.x, baseHeight + player.position.y - verticalMotionLine, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(player.position.x, baseHeight, transform.position.z);
+        }
+    }
+}
