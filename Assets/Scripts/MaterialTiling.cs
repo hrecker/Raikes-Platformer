0a87b791783523c99c948f4f661bd44efaf96aa0
hrecker <henryjrecker@gmail.com>
2016-07-02 12:55:48 -0500

    Set up automatic material tiling, created rock and grass textures

---
+++ Assets/Scripts/MaterialTiling.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MaterialTiling : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public float xTile;
+    public float yTile;
+
+	void Start ()
+    {
+        Material material = meshRenderer.material;
+        if(xTile == 0 && yTile == 0)
+        {
+            material.mainTextureScale = transform.localScale;
+        }
+        else
+        {
+            material.mainTextureScale = new Vector2(xTile, yTile);
+        }
+	}
+}
