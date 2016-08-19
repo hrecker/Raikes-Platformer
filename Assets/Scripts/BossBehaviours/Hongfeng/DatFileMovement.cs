7b768c6acff1ba20bff481eb8d2a469805eeaecc
hrecker <henryjrecker@gmail.com>
2016-08-19 16:42:07 -0500

    Set up hongfeng boss dat file rain

---
+++ Assets/Scripts/BossBehaviours/Hongfeng/DatFileMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DatFileMovement : MonoBehaviour
+{
+    public float speed;
+
+    public void Update()
+    {
+        transform.position += Vector3.down * speed;
+    }
+}
