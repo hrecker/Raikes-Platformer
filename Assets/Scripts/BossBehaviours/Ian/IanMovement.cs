5ffe38595ea2e9236a276f0ac0e8975b65987d47
hrecker <henryjrecker@gmail.com>
2016-09-01 12:46:28 -0500

    Implemented Ian boss fight

--- Assets/Scripts/BossBehaviours/Ian/IanMovement.cs
+++ Assets/Scripts/BossBehaviours/Ian/IanMovement.cs
@@ -3,40 +3,54 @@
 public class IanMovement : MonoBehaviour
 {
     public Transform player;
+    [Range(0.0f, 0.05f)]
     public float acceleration;
     public float verticalMargin;
     public float horizontalSpacing;
+    [Range(0.0f, 0.05f)]
     public float verticalDrag;
+    [Range(0.0f, 0.5f)]
+    public float maxVerticalSpeed;
 
     private float verticalSpeed;
 
     void Update()
     {
+        //verticalSpeed = 0;
+
         // accelerate to player height
         if(transform.position.y < player.position.y - verticalMargin)
         {
             verticalSpeed += acceleration;
+            if(verticalSpeed > maxVerticalSpeed)
+            {
+                verticalSpeed = maxVerticalSpeed;
+            }
         }
         else if(transform.position.y > player.position.y + verticalMargin)
         {
             verticalSpeed -= acceleration;
+            if(verticalSpeed < -maxVerticalSpeed)
+            {
+                verticalSpeed = -maxVerticalSpeed;
+            }
         }
 
         // account for drag to slow down Ian
         if(verticalSpeed > 0)
         {
             verticalSpeed -= verticalDrag;
-            if(verticalDrag < 0)
+            if(verticalSpeed < 0)
             {
-                verticalDrag = 0;
+                verticalSpeed = 0;
             }
         }
         else if (verticalSpeed < 0)
         {
             verticalSpeed += verticalDrag;
-            if (verticalDrag > 0)
+            if (verticalSpeed > 0)
             {
-                verticalDrag = 0;
+                verticalSpeed = 0;
             }
         }
 
